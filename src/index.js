b7b7964eb9fa1cd4dcba964e534c73ad68ec2f1d
AlessioFantoni <fantonialessio45@gmail.com>
2022-12-22 23:00:52 +0100

    dio cane

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
  * Author: Condotti
  * Description: https://discordjs.guide/creating-your-bot/main-file.html#running-your-application
  */
+const fs = require('node:fs');
+const path = require('node:path');
 const {token} = require("./../config/config.json");
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
-
+const { Client, Collection, Events, GatewayIntentBits} = require('discord.js');
 const client = new Client({
     "intents": [GatewayIntentBits.Guilds]
 });
@@ -16,5 +16,45 @@ client.once(Events.ClientReady, function(clientsEventObj){
     console.log('Ready!  Logged in as ' + clientsEventObj.user.tag);
 });
 
+//creazione collezione comandi
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
 // Log in to Discord with your client's token
 client.login(token);
+
+
+//executing commands
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+
